bae5aa45c9ffc02f0e5b0babebf8271e9dc88811
Hui <dennis.hui.zhou@gmail.com>
2017-12-02 13:32:03 +0800

    add start/stop script

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo systemctl start DeepOniond >/dev/null 2>&1
+sudo systemctl start DOProxy >/dev/null 2>&1
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo systemctl stop DeepOniond >/dev/null 2>&1
+sudo systemctl stop DOProxy >/dev/null 2>&1
