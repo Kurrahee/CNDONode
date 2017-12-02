175a288d40a9374bed2663459c4ebc837c0e0b1a
Hui <dennis.hui.zhou@gmail.com>
2017-12-02 13:28:34 +0800

    add proxy service

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 trap "exit 1" TERM
-PATH="`dirname \"$0\"`"
-PATH="`( cd \"$PATH\" && pwd )`"
-#echo "$PATH"
+PWD="`dirname \"$0\"`"
+PWD="`( cd \"$PWD\" && pwd )`"
+#echo "$PWD"
 
 ONIONDIR="~/.DeepOnion"
 
@@ -14,7 +14,7 @@ log()
 
 log "停止DeepOniond"
 sudo systemctl stop DeepOniond >/dev/null 2>&1
-sleep 1
+sudo systemctl stop DOProxy >/dev/null 2>&1
 
 log "安装库文件"
 yum -y install boost-devel openssl-devel libevent-devel  miniupnpc-devel libdb-cxx-devel >/dev/null 2>&1
@@ -22,18 +22,22 @@ yum -y install boost-devel openssl-devel libevent-devel  miniupnpc-devel libdb-c
 log "更新DeepOniond"
 if [ ! -d "$ONIONDIR" ];
 then
-    cp -r "$PATH/.DeepOnion" ~/
+    cp -r "${PWD}/.DeepOnion" ~/
 else
-    yes | cp "$PATH/.DeepOnion/DeepOnion.conf" $ONIONDIR
+    yes | cp "${PWD}/.DeepOnion/DeepOnion.conf" $ONIONDIR
 fi
 
-yes | cp "$PATH/DeepOniond" /usr/bin
-yes | cp "$PATH/do-proxy" /usr/bin
-yes | cp "$PATH/DeepOniond.service" /etc/systemd/system/
+yes | cp "${PWD}/DeepOniond" /usr/bin
+yes | cp "${PWD}/do-proxy" /usr/bin
+yes | cp "${PWD}/DeepOniond.service" /etc/systemd/system/
+yes | cp "${PWD}/DOProxy.service" /etc/systemd/system/
 
 log "启动DeepOniond"
 sudo systemctl daemon-reload >/dev/null 2>&1
 sudo systemctl enable DeepOniond >/dev/null 2>&1
+sudo systemctl enable DOProxy >/dev/null 2>&1
 sudo systemctl start DeepOniond >/dev/null 2>&1
+sudo systemctl start DOProxy >/dev/null 2>&1
 
 # systemctl status DeepOniond
+# systemctl status DOProxy
