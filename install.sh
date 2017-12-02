16d57605a1f4917ac2d8234a6d68dc959aa599d2
Hui <dennis.hui.zhou@gmail.com>
2017-12-02 09:42:54 +0800

    modify install script

--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
 trap "exit 1" TERM
+ONIONDIR="~/.DeepOnion"
 
-yum -y install boost-devel openssl-devel libevent-devel  miniupnpc-devel libdb-cxx-devel
+log()
+{
+    echo 1>&2 "$1"
+}
 
-cp -r .DeepOnion/ ~/
-cp ./DeepOniond /usr/bin
-cp ./DeepOniond.service /etc/systemd/system/
+log "停止DeepOniond"
+sudo systemctl stop DeepOniond >/dev/null 2>&1
+sleep 1
 
-sudo systemctl daemon-reload
-sudo systemctl enable DeepOniond
-sudo systemctl start DeepOniond
+log "安装库文件"
+yum -y install boost-devel openssl-devel libevent-devel  miniupnpc-devel libdb-cxx-devel >/dev/null 2>&1
+
+log "更新DeepOniond"
+if [ ! -d "$ONIONDIR" ];
+then
+    cp -r .DeepOnion/ ~/
+else
+    yes | cp .DeepOnion/DeepOnion.conf $ONIONDIR
+fi
+
+yes | cp ./DeepOniond /usr/bin
+yes | cp ./DeepOniond.service /etc/systemd/system/
+
+log "启动DeepOniond"
+sudo systemctl daemon-reload >/dev/null 2>&1
+sudo systemctl enable DeepOniond >/dev/null 2>&1
+sudo systemctl start DeepOniond >/dev/null 2>&1
+
+# systemctl status DeepOniond
