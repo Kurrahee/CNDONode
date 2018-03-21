157d0671e87706c193edd74371063674cfdb8f1b
Hui <dennis.hui.zhou@gmail.com>
2018-03-21 13:44:23 +0800

    change conf

--- utility/pw.go
+++ utility/pw.go
@@ -3,13 +3,24 @@ package main
 import (
 	"golang.org/x/crypto/ssh/terminal"
 	"fmt"
+	"log"
+	"os/exec"
 )
 
 
 func main() {
-	fmt.Println("Enter password: ")
+	fmt.Print("Enter password: ")
 	password, err := terminal.ReadPassword(0)
 	if err == nil {
 		fmt.Println("Password typed: " + string(password))
 	}
+
+	cmd := exec.Command("dir", "-lah")
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
+	log.Printf("Command finished with error: %v", err)
 }
